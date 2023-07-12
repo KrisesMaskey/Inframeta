6f3519d8d0c43980b7a6c9f6560e4efa53c969f8
KrisesMaskey <krisesmaskey@hotmail.com>
2023-07-12 16:22:30 -0400

    Compinents Created

--- src/src/App.js
+++ src/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './routes/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Health from './routes/Health';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/health' element={<Health />}></Route>
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+     
     </div>
   );
 }
---
+++ src/src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Home.module.css'
+import TCHealthBox from '../components/utils/TCHealthBox'
+
+function Home() {
+  return (
+    <div>
+        <h3>Home</h3>
+        <TCHealthBox></TCHealthBox>
+    </div>
+  )
+}
+
+export default Home
