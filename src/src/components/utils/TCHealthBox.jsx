6d63b74f429ad78c91e0dc715231b57931b9845c
KrisesMaskey <krisesmaskey@hotmail.com>
2023-07-12 16:21:48 -0400

    Components Created

---
+++ src/src/components/utils/TCHealthBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './TCHealthBox.module.css'
+
+function TCHealthBox() {
+  return (
+    <div className={styles.healthContainer}></div>
+  )
+}
+
+export default TCHealthBox
