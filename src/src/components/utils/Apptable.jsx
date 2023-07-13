6ac368195b6faab99b5a7fabd8ed8abf47f08a89
KrisesMaskey <krisesmaskey@hotmail.com>
2023-07-13 13:37:48 -0400

    Added table utils

---
+++ src/src/components/utils/Apptable.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Apptable.module.css'
+
+function Apptable() {
+  return (
+    <div className={styles.tableContainer}>
+        Apptable
+    </div>
+  )
+}
+
+export default Apptable
